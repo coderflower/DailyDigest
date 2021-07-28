8fd477384a8d08bdeaac9bb2277702e00bde9ab4
cailongjun <cailongjun@huami.com>
2021-07-28 14:17:06 +0800

    build |> init commit

---
+++ DailyDigest/DailyDigest/ViewController.swift
@@ -0,0 +1,19 @@
+// 
+// ViewController.swift
+// Created on 2021/7/28
+// Description <#⽂件描述#> 
+// Copyright © 2021 Huami inc. All rights reserved. 
+// @author 蔡龙君(cailongjun@huami.com)  
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
