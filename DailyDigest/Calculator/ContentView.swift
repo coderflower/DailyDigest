e8c28132949483856cd902a8cd9b9412fc00e884
cailongjun <cailongjun@huami.com>
2021-08-03 10:15:58 +0800

    build |> 添加Calculator项目

---
+++ DailyDigest/Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+// 
+// ContentView.swift
+// Created on 2021/8/3
+// Description <#⽂件描述#> 
+// Copyright © 2021 Huami inc. All rights reserved. 
+// @author 蔡龙君(cailongjun@huami.com)  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
