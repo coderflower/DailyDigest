f9b0a3c0e4dd664199af5c4973c7923a6ae5dc99
cailongjun <cailongjun@huami.com>
2021-08-03 14:43:45 +0800

    feat |> 抽取CalculatorButtonPad

--- DailyDigest/Calculator/ContentView.swift
+++ DailyDigest/Calculator/ContentView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    let row: [CalculatorButtonItem] = [
-        .digit(1), .digit(2), .digit(3), .op(.plus),
-    ]
+
     var body: some View {
-        CalculatorButtonRow(row: [
-            .digit(1), .digit(2), .digit(3), .op(.plus),
-        ])
+        CalculatorButtonPad()
+    }
+}
+
+struct CalculatorButtonPad: View {
+    
+    let pad: [[CalculatorButtonItem]] = [
+        [.command(.clear), .command(.flip), .command(.percent), .op(.divide)],
+        [.digit(7), .digit(8), .digit(9), .op(.multiply)],
+        [.digit(4), .digit(5), .digit(6), .op(.minus)],
+        [.digit(1), .digit(2), .digit(3), .op(.plus)],
+        [.digit(0), .dot, .op(.equal)]
+    ]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            ForEach(pad, id: \.self) { row in
+                CalculatorButtonRow(row: row)
+            }
+        }
     }
 }
 
